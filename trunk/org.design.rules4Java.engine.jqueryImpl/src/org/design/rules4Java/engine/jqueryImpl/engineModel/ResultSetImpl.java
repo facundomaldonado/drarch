9b877fd51df4e8f8d3a024ac1e7ef74dc4b73695
nicolasfrontini <nicolasfrontini@78db503f-7b27-0410-92dc-d98ccda72dc9>
2007-01-28 10:15:43 +0000

    Initial import.

---
+++ trunk/org.design.rules4Java.engine.jqueryImpl/src/org/design/rules4Java/engine/jqueryImpl/engineModel/ResultSetImpl.java
@@ -0,0 +1,38 @@
+package org.design.rules4Java.engine.jqueryImpl.engineModel;
+
+import org.design.rules4Java.engine.engine.engineModel.QueryResult;
+import org.design.rules4Java.engine.engine.engineModel.ResultSet;
+
+import tyRuBa.engine.Frame;
+import tyRuBa.util.ElementSource;
+
+public class ResultSetImpl implements ResultSet {
+	
+	protected ElementSource queryResult;
+	
+	protected ResultSetImpl() {
+		super();
+	}
+	
+	public boolean hasMoreElements() {
+		return queryResult.hasMoreElements();
+	}
+
+	public QueryResult next() {
+		QueryResult nextResult= createQueryResult();
+		((QueryResultImpl)nextResult).setFrame((Frame)queryResult.nextElement());
+		return nextResult;
+	}
+
+	private QueryResult createQueryResult() {
+		return new QueryResultImpl();
+	}
+	
+	public ElementSource getQueryResult() {
+		return queryResult;
+	}
+
+	public void setQueryResult(ElementSource queryResult) {
+		this.queryResult = queryResult;
+	}
+}
