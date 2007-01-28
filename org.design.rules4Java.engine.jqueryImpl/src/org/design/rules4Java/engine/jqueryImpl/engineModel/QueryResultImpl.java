f7d2ba1a874f1637c74e560528089e38aa9a057f
nicolasfrontini <nicolasfrontini@gmail.com>
2007-01-28 10:15:43 +0000

    Initial import.

---
+++ org.design.rules4Java.engine.jqueryImpl/src/org/design/rules4Java/engine/jqueryImpl/engineModel/QueryResultImpl.java
@@ -0,0 +1,42 @@
+package org.design.rules4Java.engine.jqueryImpl.engineModel;
+
+import org.design.rules4Java.engine.engine.engineModel.QueryResult;
+
+import tyRuBa.engine.Frame;
+import tyRuBa.engine.RBRepAsJavaObjectCompoundTerm;
+import tyRuBa.engine.RBVariable;
+
+public class QueryResultImpl implements QueryResult {
+
+	private Frame frame;
+	
+	protected QueryResultImpl() {
+		super();
+	}
+	
+	public String getValueOfVar(String var) {
+		String result = "";
+		if (frame.containsKey(RBVariable.make(var)))
+		{
+			Object res = frame.get(RBVariable.make(var));
+			/**
+			 * posiblemente esto no quede asi.
+			 */
+			if (res instanceof RBRepAsJavaObjectCompoundTerm){
+				result = (String)((RBRepAsJavaObjectCompoundTerm)res).getValue();
+			}
+			else{
+				result = res.toString();
+			}
+		}
+		return result.replace("#", "");
+	}
+
+	public Frame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(Frame frame) {
+		this.frame = frame;
+	}
+}
