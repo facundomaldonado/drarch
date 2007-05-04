5d47af7a7ae4156149ee70b3398a17230adf96ba
nicolasfrontini <nicolasfrontini@gmail.com>
2007-05-04 07:39:21 +0000

    Clean code and define a standard style.

--- org.design.rules4Java.util/src/org/design/rules4Java/util/UtilPlugin.java
+++ org.design.rules4Java.util/src/org/design/rules4Java/util/UtilPlugin.java
@@ -1,54 +1,58 @@
 package org.design.rules4Java.util;
 
-import org.eclipse.ui.plugin.*;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
+ * 
+ * @author nicolasfrontini@gmail.com (Nicolas Frontini)
+ * @author maldonadofacundo@gmail.com (Facundo Maldonado)
  */
 public class UtilPlugin extends AbstractUIPlugin {
 
-	//The shared instance.
-	private static UtilPlugin plugin;
-	
-	/**
-	 * The constructor.
-	 */
-	public UtilPlugin() {
-		plugin = this;
-	}
+  //The shared instance.
+  private static UtilPlugin plugin;
 
-	/**
-	 * This method is called upon plug-in activation
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+  /**
+   * The constructor.
+   */
+  public UtilPlugin() {
+    plugin = this;
+  }
 
-	/**
-	 * This method is called when the plug-in is stopped
-	 */
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		plugin = null;
-	}
+  /**
+   * This method is called upon plug-in activation
+   */
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+  }
 
-	/**
-	 * Returns the shared instance.
-	 */
-	public static UtilPlugin getDefault() {
-		return plugin;
-	}
+  /**
+   * This method is called when the plug-in is stopped
+   */
+  public void stop(BundleContext context) throws Exception {
+    super.stop(context);
+    plugin = null;
+  }
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin("org.design.rules4Java.util", path);
-	}
+  /**
+   * Returns the shared instance.
+   */
+  public static UtilPlugin getDefault() {
+    return plugin;
+  }
+
+  /**
+   * Returns an image descriptor for the image file at the given
+   * plug-in relative path.
+   *
+   * @param path the path
+   * @return the image descriptor
+   */
+  public static ImageDescriptor getImageDescriptor(String path) {
+    return AbstractUIPlugin.imageDescriptorFromPlugin("org.design.rules4Java" +
+        ".util", path);
+  }
 }
