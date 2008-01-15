50e7682389718649f3bd4ce845faf0deef7c6400
maldonadofacundo <maldonadofacundo@78db503f-7b27-0410-92dc-d98ccda72dc9>
2008-01-15 02:30:08 +0000

    Knowledgebase file is now copy into fact folder in each phase.

--- trunk/org.drarch/src/org/drarch/ui/view/wizards/NewDrarchPhaseWizard.java
+++ trunk/org.drarch/src/org/drarch/ui/view/wizards/NewDrarchPhaseWizard.java
@@ -2,6 +2,7 @@ package org.drarch.ui.view.wizards;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -25,11 +26,15 @@ import org.eclipse.ui.PlatformUI;
 public class NewDrarchPhaseWizard extends Wizard implements INewWizard {
 
 	Logger logger = Logger.getLogger(NewDrarchPhaseWizard.class.getName());
-	private NewDrarchPhaseWizardPage	page;
-	private ISelection	             selection;
+
+	private NewDrarchPhaseWizardPage page;
+
+	private ISelection selection;
+
 	private String phaseName;
-    private PropertiesConfiguration config;
-	
+
+	private PropertiesConfiguration config;
+
 	/**
 	 * Constructor for NewDrarchPhaseWizard.
 	 */
@@ -47,9 +52,8 @@ public class NewDrarchPhaseWizard extends Wizard implements INewWizard {
 	}
 
 	/**
-	 * This method is called when 'Finish' button is pressed in
-	 * the wizard. We will create an operation and run it
-	 * using wizard as execution context.
+	 * This method is called when 'Finish' button is pressed in the wizard. We
+	 * will create an operation and run it using wizard as execution context.
 	 */
 	public boolean performFinish() {
 		phaseName = page.getPhaseName();
@@ -57,10 +61,10 @@ public class NewDrarchPhaseWizard extends Wizard implements INewWizard {
 		IFolder phase;
 		IProject selectedProject;
 		if (strSelection.getFirstElement() instanceof IProject)
-			selectedProject = (IProject)strSelection.getFirstElement();
+			selectedProject = (IProject) strSelection.getFirstElement();
 		else {
-			IFile configFile = (IFile)strSelection.getFirstElement();
-			selectedProject = (IProject)configFile.getProject();
+			IFile configFile = (IFile) strSelection.getFirstElement();
+			selectedProject = (IProject) configFile.getProject();
 		}
 		phase = selectedProject.getFolder(phaseName);
 		try {
@@ -73,13 +77,18 @@ public class NewDrarchPhaseWizard extends Wizard implements INewWizard {
 
 			config = new PropertiesConfiguration(newFile);
 		} catch (CoreException e) {
-			logger.error("CoreException while trying to create resources in createPropertiesConfiguration method "
-				+ "in NewDrarchPhaseWizard.java", e);
+			logger
+					.error(
+							"CoreException while trying to create resources in createPropertiesConfiguration method "
+									+ "in NewDrarchPhaseWizard.java", e);
 			MessageDialog.openError(PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow().getShell(), getWindowTitle(), e.getMessage());
+					.getActiveWorkbenchWindow().getShell(), getWindowTitle(), e
+					.getMessage());
 		} catch (IOException e) {
-			logger.error("IOException while trying to create resources in createPropertiesConfiguration method "
-				+ "in NewDrarchPhaseWizard.java", e);
+			logger
+					.error(
+							"IOException while trying to create resources in createPropertiesConfiguration method "
+									+ "in NewDrarchPhaseWizard.java", e);
 		} catch (ConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -90,108 +99,34 @@ public class NewDrarchPhaseWizard extends Wizard implements INewWizard {
 	}
 
 	/**
-     * @param phase
-     */
-    private void createConfigFileForPhase(IFolder phase) {
-    	try {
-	    	
-	    	String flabotFilePath = phase.getLocation().toString();
-	    	String flabotFileName = flabotFilePath + "/flabot/" + page.getFlabotFileName();
-	    	String workingSetName = page.getWorkingSetName();
-	    	boolean isInteractive = page.isInteractive();
-	    	
-	        config.setProperty("newPhase.projectName", phase.getProject().getName());
-		    config.setProperty("newPhase.name", phaseName);
-		    config.setProperty("newPhase.flabotFileName", flabotFileName);
-		    config.setProperty("newPhase.workingSetName", workingSetName);
-		    config.setProperty("newPhase.isInteractive", String.valueOf(isInteractive));
-		    config.save();
-		    
-        } catch (ConfigurationException e) {
-			logger.error("ConfigurationException while trying to create resources in createPropertiesConfiguration method " + 
-					"in NewDrarchPhaseWizard.java", e);
-		} 
-    }
-
-	/**
-     * @param phase
-     */
-//    private void importFactFiles2newProject(IFolder phase) {
-//    	IFolder factsFolder = phase.getFolder("facts");
-//    	try {
-//    		Bundle drarchPlugin = DrarchPlugin.getDefault().getBundle();
-//	    	Enumeration<String> rulesFiles = drarchPlugin.getEntryPaths("/resources/facts/");
-//	    	Map<String, URL> drarchRuleFiles = filterFilesByPattern(drarchPlugin, rulesFiles, ".rub");
-//	    	
-//	    	importFiles2Folder(drarchRuleFiles, factsFolder);
-//	    	
-//    	} catch (URISyntaxException e) {
-//    		logger.error("URISyntaxException while importing facts files",e);
-//        } catch (IOException e) {
-//        	logger.error("IOException while importing facts files",e);
-//        }
-//    }
-//
-//	/**
-//     * 
-//     */
-//    private void importRulesFiles2newProject(IFolder phase) {
-//    	IFolder rulesFolder = phase.getFolder("rules");
-//    	try {
-//    		Bundle drarchPlugin = DrarchPlugin.getDefault().getBundle();
-//	    	Enumeration<String> rulesFiles = drarchPlugin.getEntryPaths("/resources/rules/");
-//	    	Map<String, URL> drarchRuleFiles = filterFilesByPattern(drarchPlugin, rulesFiles, ".drarch");
-//	    	
-//	    	importFiles2Folder(drarchRuleFiles, rulesFolder);
-//	    	
-//    	} catch (URISyntaxException e) {
-//    		logger.error("URISyntaxException while importing rules files",e);
-//        } catch (IOException e) {
-//        	logger.error("IOException while importing rules files",e);
-//        }
-//    }
-//
-//	/**
-//     * @param files
-//     * @param destiniFolder
-//     * @throws URISyntaxException
-//     * @throws IOException
-//     */
-//    private void importFiles2Folder(Map<String, URL> files, IFolder destiniFolder) throws URISyntaxException, IOException {
-//	    for (Iterator<String> iterator = files.keySet().iterator() ; iterator.hasNext();) {
-//	    	String fileName = iterator.next();
-//	    	URL url = (URL)files.get(fileName);
-//	    	
-//	    	URI sourceFileURI = FileLocator.toFileURL(url).toURI();
-//
-//	        File sourceFile = new File(sourceFileURI);
-//	        
-//	        URI destiniFolderURI = destiniFolder.getLocationURI();
-//	        String destiniFileName = destiniFolderURI.getPath() + "/" + fileName.substring(fileName.lastIndexOf("/")) ;
-//	        File destiniFile = new File(destiniFileName);
-//	        FileUtils.copyFile(sourceFile, destiniFile);
-//	    }
-//    }
-//
-//	/**
-//     * @param rulesFiles
-//     * @return
-//     */
-//    private Map<String, URL> filterFilesByPattern(Bundle drarchPlugin, Enumeration<String> files, String pattern) {
-//    	Map<String, URL> result = new HashMap<String, URL>();
-//    	while (files.hasMoreElements()) {
-//    		String file = files.nextElement();
-//    		if (file.endsWith(pattern)) {
-//    			URL fileUrl = drarchPlugin.getEntry(file);
-//    			result.put(file, fileUrl);
-//    		}
-//    	}
-//    	return result;
-//    }
-
-	/**
-	 * 
+	 * @param phase
 	 */
+	private void createConfigFileForPhase(IFolder phase) {
+		try {
+
+			String flabotFilePath = phase.getLocation().toString();
+			String flabotFileName = flabotFilePath + "/flabot/"
+					+ page.getFlabotFileName();
+			String workingSetName = page.getWorkingSetName();
+			boolean isInteractive = page.isInteractive();
+
+			config.setProperty("newPhase.projectName", phase.getProject()
+					.getName());
+			config.setProperty("newPhase.name", phaseName);
+			config.setProperty("newPhase.flabotFileName", flabotFileName);
+			config.setProperty("newPhase.workingSetName", workingSetName);
+			config.setProperty("newPhase.isInteractive", String
+					.valueOf(isInteractive));
+			config.save();
+
+		} catch (ConfigurationException e) {
+			logger
+					.error(
+							"ConfigurationException while trying to create resources in createPropertiesConfiguration method "
+									+ "in NewDrarchPhaseWizard.java", e);
+		}
+	}
+
 	private void createDirectoryStructure(IFolder phase) {
 		try {
 			IFolder rulesFolder = phase.getFolder("rules");
@@ -203,40 +138,59 @@ public class NewDrarchPhaseWizard extends Wizard implements INewWizard {
 
 			IFolder flabotFolder = phase.getFolder("flabot");
 			flabotFolder.create(true, false, null);
-			
 
 		} catch (CoreException e) {
-			logger.error("CoreException while trying to create resources in createDirectoryStructure method " + "in NewDrarchPhaseWizard.java", e);
+			logger
+					.error(
+							"CoreException while trying to create resources in createDirectoryStructure method "
+									+ "in NewDrarchPhaseWizard.java", e);
 		}
 	}
 
 	/**
-     * @param factsFolder
-     */
-    private void createPhaseKnowledgeBase(IFolder factsFolder) {
-    	String knowledgeBaseFileName = phaseName + ".rub";
-	    String phaseKnowledgeBaseFilePath = factsFolder.getLocation().toString() + 
-	    	String.valueOf(Path.SEPARATOR) + knowledgeBaseFileName;
-	    File phaseKnowledgeBaseFile = new File(phaseKnowledgeBaseFilePath);
-	    try {
-	        FileUtils.touch(phaseKnowledgeBaseFile);
-	        factsFolder.refreshLocal(IFolder.DEPTH_INFINITE, null);
-	        
-	        config.setProperty("newPhase.knowledgeBaseFile", knowledgeBaseFileName);
-	        config.save();
-	        
-        } catch (IOException e) {
-        	logger.error(e.getClass().getName(), e);
-        } catch (CoreException e) {
-        	logger.error(e.getClass().getName(), e);
-        } catch (ConfigurationException e) {
-        	logger.error(e.getClass().getName(), e);
-        }
-    }
+	 * @param factsFolder
+	 */
+	private void createPhaseKnowledgeBase(IFolder factsFolder) {
+		try {
+			IProject project = factsFolder.getProject();
+			IFolder factsdepot = project.getFolder("Facts Depot");
+			IFile knowledgeBase = factsdepot.getFile("KnowledgeBase.rub");
+			System.out.println(knowledgeBase.isAccessible());
+
+			File sourceFile = new File(knowledgeBase.getLocationURI());
+
+			URI destiniFolderURI = factsFolder.getLocationURI();
+			File destiniFile = new File(destiniFolderURI.getPath()
+					+ "/KnowledgeBase.rub");
+
+			FileUtils.copyFile(sourceFile, destiniFile);
+
+			String knowledgeBaseFileName = phaseName + ".rub";
+			String phaseKnowledgeBaseFilePath = factsFolder.getLocation()
+					.toString()
+					+ String.valueOf(Path.SEPARATOR) + knowledgeBaseFileName;
+			File phaseKnowledgeBaseFile = new File(phaseKnowledgeBaseFilePath);
+
+			FileUtils.touch(phaseKnowledgeBaseFile);
+			factsFolder.refreshLocal(IFolder.DEPTH_INFINITE, null);
+
+			config.setProperty("newPhase.knowledgeBaseFile",
+					knowledgeBaseFileName);
+			config.save();
+
+		} catch (IOException e) {
+			logger.error(e.getClass().getName(), e);
+		} catch (CoreException e) {
+			logger.error(e.getClass().getName(), e);
+		} catch (ConfigurationException e) {
+			logger.error(e.getClass().getName(), e);
+		}
+	}
 
 	/**
-	 * We will accept the selection in the workbench to see if
-	 * we can initialize from it.
+	 * We will accept the selection in the workbench to see if we can initialize
+	 * from it.
+	 * 
 	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
 	 */
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
